1482366925f722892c56d46cc8445a7365ac8e10
LAPTOP-FITMV6KM\minse <howCanIFind@github.com>
2022-05-11 14:59:34 +0900

    스택 수열 1874
    스택 수열 1874 풀이 완료

---
+++ src/Rotating_queue_1021.java
@@ -0,0 +1,6 @@
+public class Rotating_queue_1021 {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/stackSequence_1874.java
+++ src/stackSequence_1874.java
@@ -1,7 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class stackSequence_1874 {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        Stack<Integer> stack = new Stack<>();
+
+        int N =Integer.parseInt(br.readLine());
+
+        int last_value = 0;
+        while (N -- > 0) {
+            int value = Integer.parseInt(br.readLine());
+
+            if(last_value < value) {
+                for(int i=last_value+1; i<=value; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+                last_value = value;
+            } else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
     }
 }
