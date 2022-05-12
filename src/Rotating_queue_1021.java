91d743f4ab889712769210d6da58cd53e1c94984
LAPTOP-FITMV6KM\minse <howCanIFind@github.com>
2022-05-12 15:40:08 +0900

    자바 괄호_9012
    bracket_9012 풀이 완료

--- src/Rotating_queue_1021.java
+++ src/Rotating_queue_1021.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
 public class Rotating_queue_1021 {
 
-    public static void main(String[] args) {
+    public static LinkedList<Integer> deque = new LinkedList<Integer>();
+    int count=0;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken()); // 큐의 크기
+        int M = Integer.parseInt(st.nextToken()); // 뽑으려는 숫자의 개수
+
+        for(int i=1; i<=N; i++) {
+            deque.offer(i);
+        }
+
+        int[] sequence = new int[M];
 
     }
 }
