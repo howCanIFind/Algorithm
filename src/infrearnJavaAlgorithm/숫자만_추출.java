4d7fd3ca81acac612bf90ef82d452c9bb33d67bc
howCanIFind <second9000@naver.com>
2022-12-01 06:33:23 +0900

    숫자만 추출

---
+++ src/infrearnJavaAlgorithm/숫자만_추출.java
@@ -0,0 +1,31 @@
+package infrearnJavaAlgorithm;
+
+import java.util.Scanner;
+
+public class 숫자만_추출 {
+
+    // 아스키 넘버 사용
+    public static int solution1(String str) {
+        int answer = 0;
+        for (char x : str.toCharArray()) {
+            if (x >= 48 && x <= 57) answer = answer * 10 + (x - 48);
+        }
+        return answer;
+    }
+
+    // isDigit, parseInt 사용
+    public static int solution2(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) answer += x;
+        }
+        return Integer.parseInt(answer);
+    }
+
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution1(str));
+    }
+
+}
