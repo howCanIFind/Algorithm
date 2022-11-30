54f5b185e893ae8f28b3727e5300ef54bc879013
howCanIFind <second9000@naver.com>
2022-11-30 21:16:54 +0900

    중복문자제거

---
+++ src/infrearnJavaAlgorithm/중복문자제거.java
@@ -0,0 +1,21 @@
+package infrearnJavaAlgorithm;
+
+import java.util.Scanner;
+
+public class 중복문자제거 {
+    public static String solution(String str) {
+        String answer = "";
+
+        for (int i=0; i<str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
+        }
+
+        return answer;
+    }
+
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+}
