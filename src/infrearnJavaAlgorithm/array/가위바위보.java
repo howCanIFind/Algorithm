bbeebe70563c02ee510f594cd22d585ee58250f6
howCanIFind <second9000@naver.com>
2022-12-01 20:59:20 +0900

    피보나치 수열

--- src/infrearnJavaAlgorithm/array/가위바위보.java
+++ src/infrearnJavaAlgorithm/array/가위바위보.java
@@ -4,32 +4,21 @@ import java.util.Scanner;
 
 public class 가위바위보 {
 
-    public static String solution(int n, int[] a, int[] b) {
-        StringBuilder answer = new StringBuilder();
-        for (int i=0; i<n; i++) {
-            if (a[i]==b[i]) answer.append("D");
-            else if (a[i] == 1 && b[i] == 3) answer.append("A");
-            else if (a[i] == 2 && b[i] == 1) answer.append("A");
-            else if (a[i] == 3 && b[i] == 2) answer.append("A");
-            else answer.append("B");
+    public static int[] solution(int n) {
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i=2; i<n; i++) {
+            answer[i] = answer[i-2]+answer[i-1];
         }
-
-        return answer.toString();
+        return answer;
     }
 
     public static void main (String[]args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n];
-        int[] b = new int[n];
-        for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
-        }
-        for (int i=0; i<n; i++) {
-            b[i] = sc.nextInt();
-        }
-        for (char x : solution(n, a, b).toCharArray()) {
-            System.out.println(x);
+        for (int x : solution(n)) {
+            System.out.print(x+" ");
         }
     }
 }
