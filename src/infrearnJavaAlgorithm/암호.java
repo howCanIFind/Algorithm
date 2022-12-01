63db590052a26012a3254dad228c14ce9cd96364
howCanIFind <second9000@naver.com>
2022-12-01 11:13:36 +0900

    암호

---
+++ src/infrearnJavaAlgorithm/암호.java
@@ -0,0 +1,26 @@
+package infrearnJavaAlgorithm;
+
+import java.util.Scanner;
+
+public class 암호 {
+
+    public static String solution(int n, String str) {
+        String answer = "";
+        for (int i=0; i<n; i++) {
+            String tmp = str.substring(0,7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer += (char) num;
+            str = str.substring(7);
+        }
+
+        return answer;
+    }
+
+
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        System.out.println(solution(n, str));
+    }
+}
