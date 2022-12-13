488ae8437af1cc5d854cfeb2e7bcc44732af2e93
howCanIFind <second9000@naver.com>
2022-12-13 16:57:38 +0900

    응급실

--- src/infrearnJavaAlgorithm/stackQueue/응급실.java
+++ src/infrearnJavaAlgorithm/stackQueue/응급실.java
@@ -1,4 +1,48 @@
 package infrearnJavaAlgorithm.stackQueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class 응급실 {
+    static class Person {
+        int id;
+        int priority;
+        public Person(int id, int priority) {
+            this.id = id;
+            this.priority = priority;
+        }
+    }
+    public static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Queue<Person> queue = new LinkedList<>();
+        for (int i=0; i<n; i++) {
+            queue.offer(new Person(i, arr[i]));
+        }
+        while (!queue.isEmpty()) {
+            Person tmp = queue.poll();
+            for (Person x : queue) {
+                if (x.priority>tmp.priority) {
+                    queue.offer(tmp);
+                    tmp=null;
+                    break;
+                }
+            }
+            if (tmp!=null) {
+                answer++;
+                if (tmp.id==m) return answer;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(n, m, arr));
+    }
 }
