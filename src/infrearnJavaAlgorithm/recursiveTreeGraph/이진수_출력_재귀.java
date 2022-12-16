166256d23bee4fef9d8c80c659eecb60277e32d4
howCanIFind <second9000@naver.com>
2022-12-16 09:23:27 +0900

    이진수 출력(재귀)

--- src/infrearnJavaAlgorithm/recursiveTreeGraph/이진수_출력_재귀.java
+++ src/infrearnJavaAlgorithm/recursiveTreeGraph/이진수_출력_재귀.java
@@ -1,14 +1,14 @@
 package infrearnJavaAlgorithm.recursiveTreeGraph;
 
-public class 재귀함수_스택프레임 {
+public class 이진수_출력_재귀 {
     public static void DFS(int n) {
         if (n==0) return;
         else {
-            DFS(n-1);
-            System.out.println(n+" ");
+            DFS(n/2);
+            System.out.println(n%2+" ");
         }
     }
     public static void main(String[] args) {
-        DFS(3);
+        DFS(11);
     }
 }
