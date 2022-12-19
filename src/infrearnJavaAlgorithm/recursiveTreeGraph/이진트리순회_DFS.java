c1cf94baa4ccf5a10cc514b5ed5f9b530aae9d51
howCanIFind <second9000@naver.com>
2022-12-19 15:37:58 +0900

    이진트리순회(DFS:Depth-First Search)

--- src/infrearnJavaAlgorithm/recursiveTreeGraph/이진트리순회_DFS.java
+++ src/infrearnJavaAlgorithm/recursiveTreeGraph/이진트리순회_DFS.java
@@ -1,10 +1,33 @@
 package infrearnJavaAlgorithm.recursiveTreeGraph;
 
 public class 이진트리순회_DFS {
-
-    public static void DFS(int n) {
-
+    static class Node {
+        int data;
+        Node lt, rt;
+        public Node(int val) {
+            data = val;
+            lt = rt =null;
+        }
     }
+
+    public void DFS(Node root) {
+        if (root == null) return;
+        else {
+            DFS(root.lt);
+            System.out.print(root.data+" ");
+            DFS(root.rt);
+        }
+    }
+    Node root;
     public static void main(String[] args) {
+        이진트리순회_DFS tree = new 이진트리순회_DFS();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.DFS(tree.root);
     }
 }
