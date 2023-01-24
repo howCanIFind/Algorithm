d4262698cf86f571eb3c1309f7343216d7a38c19
howCanIFind <second9000@naver.com>
2023-01-24 23:24:59 +0900

    친구인가

--- src/infrearnJavaAlgorithm/greedy/친구인가_Uion_find.java
+++ src/infrearnJavaAlgorithm/greedy/친구인가_Uion_find.java
@@ -1,4 +1,23 @@
 package infrearnJavaAlgorithm.greedy;
 
+import java.util.Scanner;
+
 public class 친구인가_Uion_find {
+    static int[] unf;
+    public static int Find(int v) {
+        if (v==unf[v]) return v;
+        else return unf[v] = Find(unf[v]);
+    }
+
+    public static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa != fb) unf[fa] = fb;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+
 }
