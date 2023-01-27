681994c1996a061c277e7c86588673aeef46cf40
howCanIFind <second9000@naver.com>
2023-01-27 01:11:13 +0000

    계단오르기

--- src/infrearnJavaAlgorithm/dynamic/계단오르기.java
+++ src/infrearnJavaAlgorithm/dynamic/계단오르기.java
@@ -1,4 +1,19 @@
 package infrearnJavaAlgorithm.dynamic;
 
+import java.util.Scanner;
+
 public class 계단오르기 {
+    static int[] dy;
+    public static int solution(int n) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dy = new int[n+1];
+        System.out.println(solution(n));
+    }
 }
