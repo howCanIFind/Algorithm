9896a8935d491624a012bce1073405f906828ab2
howCanIFind <second9000@naver.com>
2023-01-31 22:30:56 +0900

    동다리건너기

--- src/infrearnJavaAlgorithm/dynamic/돌다리_건너기.java
+++ src/infrearnJavaAlgorithm/dynamic/돌다리_건너기.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class 돌다리_건너기 {
     static int[] dy;
     public static int solution(int n) {
-        int answer = 0;
-        return answer;
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i=3; i<=n; i++) {
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+        return dy[n+1];
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
